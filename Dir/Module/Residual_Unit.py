155a5cb092f8e69167e190e472c445d5ddd3578f
deszczowaa <maja.szrubarczyk@onet.pl>
2022-06-27 11:39:17 +0200

    First commit

---
+++ Dir/Module/Residual_Unit.py
@@ -0,0 +1,49 @@
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Add
+
+"""
+Residual Unit is implemented above proposed Residual Block
+input >> BN >> ReLU >> Conv2DLayer(1,1)
+      >> BN >> ReLU >> Conv2DLayer(3,3)
+      >> BN >> ReLU >> Conv2DLayer(1,1) + identify
+      >> output
+"""
+
+def Residual_Unit(input, in_channel, out_channel, stride=1):
+
+    """
+    :param input: The input of the Residual_Unit. Should be a 4D array like (batch_num, img_len, img_len, channel_num)
+    :param in_channel: The 4-th dimension (channel number) of input matrix. For example, in_channel=3 means the input contains 3 channels.
+    :param out_channel: The 4-th dimension (channel number) of output matrix. For example, out_channel=5 means the output contains 5 channels (feature maps).
+    :param stride: Integer. The number of pixels to move between 2 neighboring receptive fields.
+    """
+
+    # initialize as the input (identity) data
+    shortcut = input
+    shortcut = Conv2D(out_channel, (1, 1), padding='same', strides=stride)(shortcut)
+
+    # RestNet module
+    x = BatchNormalization()(input)
+    x = Activation('relu')(x)
+    x = Conv2D(in_channel, (1, 1))(x)
+
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(in_channel, (3, 3), padding='same', strides=stride)(x)
+
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(out_channel, (1, 1), padding='same')(x)
+
+    # identity
+    x = Add()([x, shortcut])
+
+    return x
+
+
+
+
+
+
